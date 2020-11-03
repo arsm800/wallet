033b53f7f517c2a67b6c3b6e9d8afe4cadfe19e9
Andrew Smith-Mui <asmithmui@gmail.com>
2020-11-03 12:59:22 -0500

    Created 'wallet.py' universal wallet script.

---
+++ .ipynb_checkpoints/wallet-checkpoint.py
@@ -0,0 +1,11 @@
+import subprocess
+import json
+
+command = './derive -g --mnemonic="sunny carpet addict journey solid noodle grab exercise limb puppy process diary" --cols=path,address,privkey,pubkey --format=json'
+
+p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+output, err = p.communicate()
+p_status = p.wait()
+
+keys = json.loads(output)
+print(keys)
