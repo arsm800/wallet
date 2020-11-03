66dc8af80d3ac109fc3f96bd57b2405319e4429d
Andrew Smith-Mui <asmithmui@gmail.com>
2020-11-03 15:06:35 -0500

    Generated mnemonic variable from .env file.

--- wallet.py
+++ wallet.py
@@ -1,6 +1,13 @@
 import subprocess
 import json
+import os
+import dotenv
+
 from constants import *
+
+dotenv.load_dotenv()
+
+mnemonic = os.getenv("MNEMONIC", "mnemonic")
 
 command = './derive -g --mnemonic="sunny carpet addict journey solid noodle grab exercise limb puppy process diary" --cols=path,address,privkey,pubkey --format=json'
 
