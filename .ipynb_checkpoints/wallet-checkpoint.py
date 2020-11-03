b1c35c6a372833af56524ad63fa42f1137099371
Andrew Smith-Mui <asmithmui@gmail.com>
2020-11-03 16:18:38 -0500

    Created function to derive wallet keys and create dictionary of keys for BTC, ETH, and BTCTEST.

--- .ipynb_checkpoints/wallet-checkpoint.py
+++ .ipynb_checkpoints/wallet-checkpoint.py
@@ -1,3 +1,4 @@
+# Imports
 import subprocess
 import json
 import os
@@ -7,13 +8,28 @@ from constants import *
 
 dotenv.load_dotenv()
 
+# Mnemonic variable for cryptowallet private key stored in .env file
 mnemonic = os.getenv("mnemonic", "mnemonic")
 
+# Establish variable for derive command 
 command = './derive -g --mnemonic=mnemonmic --cols=path,address,privkey,pubkey --numderive=12 --format=json --coin'
 
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-output, err = p.communicate()
-p_status = p.wait()
+# Create function to derive wallets
+def derive_wallets():
 
-keys = json.loads(output)
-print(keys)
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, err = p.communicate()
+    p_status = p.wait()
+
+    keys = json.loads(output)
+    
+    # Create dictionary to assign 4 keys to each currency created in constants.py
+    coins = {
+             BTC: [keys[0], keys[1], keys[2], keys[3]],
+             ETH: [keys[4], keys[5], keys[6], keys[7]],
+             BTCTEST: [keys[8], keys[9], keys[10], keys[11]]
+            }
+    
+    print(coins)
+    
+derive_wallets()
